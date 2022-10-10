8f845c6c5e2d0fa532ffe1a194889c5b348530f3
wandersoncbjr <wandersoncbjr2@gmail.com>
2022-10-10 14:50:01 -0300

    add import do app.css

--- src/App.js
+++ src/App.js
@@ -1,36 +1,28 @@
 import React, { useEffect, useState } from 'react'
 import api from './services/api';
 import LinhaDeFilmes from './components/LinhaDeFilmes';
+import './App.css'
 
 
 function App() {
 
-    const[listaFilmes, setFilmesLista] = useState([]);
-
+    const [listaFilmes, setFilmesLista] = useState([]);
 
     useEffect(() => {
-
         const carregarTudo = async () => {
             let list = await api.pegandoLista();
             console.log(list);
             setFilmesLista(list);
         }
-
         carregarTudo();
-
-    }, []);
+    }, []); 
 
     return (
         <div className="App">
             <section className='listas'>
-                {listaFilmes.map((item, key)=>(
-            
-
-                    
+                {listaFilmes.map((item, key) => (
                     <div>
-                        <LinhaDeFilmes key={key} title={item.titles} items={item.items}/>
-
-                      
+                        <LinhaDeFilmes key={key} title={item.titles} items={item.items} />
                     </div>
                 ))}
             </section>
