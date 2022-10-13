376b20015605853302085872c0e2c1985c17b5cf
wandersoncbjr <wandersoncbjr2@gmail.com>
2022-10-13 18:17:38 -0300

    renomeando os arquivos JS para JSX

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import './index.css'
+
+
+export default () =>{
+
+    return(
+       <>
+
+<footer class="pie-pagina">
+        <div class="grupo-1">
+            <div class="box">
+                <figure>
+                    {/* <a href="#">
+                        <img src="img/logotipo-sleedw.svg" alt="Logo de SLee Dw">
+                    </a> */}
+                </figure>
+            </div>
+            <div class="box">
+                <h2>SOBRE MIM</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, ipsa?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, ipsa?</p>
+            </div>
+            <div class="box">
+                <h2>CONTATOS</h2>
+                <div class="red-social">
+                    <a href="#" class="fa fa-facebook"></a>
+                    <a href="#" class="fa fa-instagram"></a>
+                    <a href="#" class="fa fa-twitter"></a>
+                    <a href="#" class="fa fa-youtube"></a>
+                </div>
+            </div>
+        </div>
+        <div class="grupo-2">
+            <small>&copy; 2022 <b>the movie db</b> - Todos os Direitos Reservados Para Netflix.</small>
+        </div>
+    </footer>
+       </>
+    )
+
+};
