6170fbf638a5c5a98689dee23ec94e5b7ea6daa3
wandersoncbjr <wandersoncbjr2@gmail.com>
2022-10-16 18:14:35 -0300

    add estilizado footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,40 +2,41 @@ import React from "react";
 import './index.css'
 
 
-export default () =>{
+export default () => {
 
-    return(
-       <>
+    return (
+        <>
 
-<footer class="pie-pagina">
-        <div class="grupo-1">
-            <div class="box">
-                <figure>
-                    {/* <a href="#">
+            <footer class="pie-pagina">
+                <div class="grupo-1">
+                    <div class="box">
+                        <figure>
+                            {/* <a href="#">
                         <img src="img/logotipo-sleedw.svg" alt="Logo de SLee Dw">
                     </a> */}
-                </figure>
-            </div>
-            <div class="box">
-                <h2>SOBRE MIM</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, ipsa?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, ipsa?</p>
-            </div>
-            <div class="box">
-                <h2>CONTATOS</h2>
-                <div class="red-social">
-                    <a href="#" class="fa fa-facebook"></a>
-                    <a href="#" class="fa fa-instagram"></a>
-                    <a href="#" class="fa fa-twitter"></a>
-                    <a href="#" class="fa fa-youtube"></a>
+                        </figure>
+                    </div>
+                    <div class="box">
+
+                        <h2>CONTATOS</h2>
+                        <div class="red-social">
+                            <a href="https://github.com/wandersoncbjr" class="fa fa-github"></a>
+                            <a href="https://www.linkedin.com/in/wanderson-santos-7717651ba/" class="fa fa-linkedin"></a>
+                            <a href="https://www.instagram.com/wanderson_santos013/" class="fa fa-instagram"></a>
+
+                        </div>
+                        <div className="email">
+                            <div> <h4>wadersoncbjr2@gmail.com</h4> </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-        <div class="grupo-2">
-            <small>&copy; 2022 <b>the movie db</b> - Todos os Direitos Reservados Para Netflix.</small>
-        </div>
-    </footer>
-       </>
+                <div class="box">
+                </div>
+                <div class="grupo-2">
+                    <small>&copy; 2022 <b>The Movie DB</b> - Todos os Direitos Reservados Para Netflix.</small>
+                </div>
+            </footer>
+        </>
     )
 
 };
